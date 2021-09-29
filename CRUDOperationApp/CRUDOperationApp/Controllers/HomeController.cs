7da3556c7a4fa919424dd0fb92bc79ef25125bbd
Hung08 <hung.187pm20486@vanlanguni.vn>
2021-09-29 19:49:29 +0700

    Update Project

--- CRUDOperationApp/CRUDOperationApp/Controllers/HomeController.cs
+++ CRUDOperationApp/CRUDOperationApp/Controllers/HomeController.cs
@@ -3,28 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CRUDOperationApp.Models;
 
 namespace CRUDOperationApp.Controllers
 {
     public class HomeController : Controller
     {
+        private DemoDBEntities db = new DemoDBEntities();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult GetAllStudents()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var sl = db.Students.ToList();
+            return Json(sl, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult AddStudent(Student student)
         {
-            ViewBag.Message = "Your contact page.";
+            db.Students.Add(student);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult UpdateStudent(Student student)
+        {
+            var s = db.Students.SingleOrDefault(x => x.Id == student.Id);
+            s.Name = student.Name;
+            s.Age = student.Age;
+            s.Department = student.Department;
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteStudent(Student student)
+        {
+            var s = db.Students.SingleOrDefault(x => x.Id == student.Id);
+            db.Students.Remove(s);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ CRUDOperationApp/CRUDOperationApp/Scripts/Student.js
@@ -0,0 +1,88 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module('myApp', []);
+app.controller('myController', function ($scope, $http) {
+
+    $scope.GetAllStudents = function() {
+        $http({
+            method: "get",
+            url: "/Home/GetAllStudents"
+        }).then(function (response) {
+            $scope.students = response.data;
+        }, function () {
+                alert("Lỗi !!!");
+        });
+    }
+
+    $scope.InsertStudent = function () {
+        var type = $("#insert").val();
+        if (type == "Add") {
+            $scope.student = new Object();
+            $scope.student.Name = $scope.SName;
+            $scope.student.Age = $scope.SAge;
+            $scope.student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "/Home/AddStudent",
+                datatype: "json",
+                data: JSON.stringify($scope.student)
+            }).then(function (response) {
+                $scope.GetAllStudents();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+                alertify.success('Thêm thành công !!!');
+            }, function () {
+                alertify.error('Lỗi !!!');
+            });
+        }
+        else {
+            $scope.student = new Object();
+            $scope.student.Id = $scope.SId;
+            $scope.student.Name = $scope.SName;
+            $scope.student.Age = $scope.SAge;
+            $scope.student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "/Home/UpdateStudent",
+                datatype: "json",
+                data: JSON.stringify($scope.student)
+            }).then(function (response) {
+                $scope.GetAllStudents();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+                $("#insert").val("Add");
+                alertify.success('Cập nhật thành công !!!');
+            }, function () {
+                alertify.error('Lỗi !!!');
+            });
+        }
+    }
+
+    $scope.UpdateStudent = function (item) {
+        $scope.SId = item.Id;
+        $scope.SName = item.Name;
+        $scope.SAge = item.Age;
+        $scope.SDepartment = item.Department;
+        $("#insert").val("Save");
+    }
+
+    $scope.DeleteStudent = function (item) {
+        alertify.confirm("Thông báo","Bạn có muốn xóa không ???",
+            function () {
+                $http({
+                    method: "get",
+                    url: "/Home/DeleteStudent/" + item.Id,
+                }).then(function (response) {
+                    $scope.GetAllStudents();
+                    alertify.success('Xóa thành công !!!');
+                }, function () {
+                    alertify.error('Lỗi !!!');
+                });
+            },
+            function () {
+                alertify.error('Đã hủy');
+            });
+    }
+});
